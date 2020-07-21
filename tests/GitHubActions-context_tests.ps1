36de88446bd67f562c52affe634cb7a6bac2818e
Eugene Bekker <ebekker@users.noreply.github.com>
2020-07-20 20:38:07 -0400

    adding mocked tests for Action Context

---
+++ tests/GitHubActions-context_tests.ps1
@@ -0,0 +1,85 @@
+
+Import-Module Pester
+
+Import-Module $PSScriptRoot/../GitHubActions
+
+if (-not (Get-Variable EOL -ErrorAction SilentlyContinue)) {
+    Set-Variable -Scope Script -Option Constant -Name EOL -Value ([System.Environment]::NewLine)
+}
+
+## These two are borrowed from:
+##   https://github.com/chriskuech/functional/blob/master/functional.psm1#L42
+function isPsCustomObject($v) {
+    $v.PSTypeNames -contains 'System.Management.Automation.PSCustomObject'
+}
+function recursiveEquality($a, $b) {
+    if ($a -is [array] -and $b -is [array]) {
+        Write-Debug "recursively test arrays '$a' '$b'"
+        if ($a.Count -ne $b.Count) {
+            return $false
+        }
+        $inequalIndexes = 0..($a.Count - 1) | ? { -not (recursiveEquality $a[$_] $b[$_]) }
+        return $inequalIndexes.Count -eq 0
+    }
+    if ($a -is [hashtable] -and $b -is [hashtable]) {
+        Write-Debug "recursively test hashtable '$a' '$b'"
+        $inequalKeys = $a.Keys + $b.Keys `
+        | Sort-Object -Unique `
+        | ? { -not (recursiveEquality $a[$_] $b[$_]) }
+        return $inequalKeys.Count -eq 0
+    }
+    if ((isPsCustomObject $a) -and (isPsCustomObject $b)) {
+        Write-Debug "a is pscustomobject: $($a -is [psobject])"
+        Write-Debug "recursively test objects '$a' '$b'"
+        $inequalKeys = $a.psobject.Properties + $b.psobject.Properties `
+        | % Name `
+        | Sort-Object -Unique `
+        | ? { -not (recursiveEquality $a.$_ $b.$_) }
+        return $inequalKeys.Count -eq 0
+    }
+    Write-Debug "test leaves '$a' '$b'"
+    return (($null -eq $a -and $null -eq $b) -or ($null -ne $a -and $null -ne $b -and $a.GetType() -eq $b.GetType() -and $a -eq $b))
+}
+
+Describe 'Get-ActionContext' {
+    It 'Mock environment values' {
+        $env:GITHUB_EVENT_NAME = 'GITHUB_EVENT_NAME'
+        $env:GITHUB_SHA = 'GITHUB_SHA'
+        $env:GITHUB_REF = 'GITHUB_REF'
+        $env:GITHUB_WORKFLOW = 'GITHUB_WORKFLOW'
+        $env:GITHUB_ACTION = 'GITHUB_ACTION'
+        $env:GITHUB_ACTOR = 'GITHUB_ACTOR'
+        $env:GITHUB_JOB = 'GITHUB_JOB'
+        $env:GITHUB_RUN_NUMBER = '999'
+        $env:GITHUB_RUN_ID = '888'
+
+        $eventPath = "$($PSScriptRoot)./payload-sample1.json"
+        $env:GITHUB_EVENT_PATH = $eventPath
+
+        $context = Get-ActionContext
+
+        $context.EventName | Should -Be 'GITHUB_EVENT_NAME'
+        $context.Sha | Should -Be 'GITHUB_SHA'
+        $context.Ref | Should -Be 'GITHUB_REF'
+        $context.Workflow | Should -Be 'GITHUB_WORKFLOW'
+        $context.Action | Should -Be 'GITHUB_ACTION'
+        $context.Actor | Should -Be 'GITHUB_ACTOR'
+        $context.Job | Should -Be 'GITHUB_JOB'
+        $context.RunNumber | Should -Be 999
+        $context.RunId | Should -Be 888
+
+        $payldJson = $context.Payload | ConvertTo-Json -Depth 7
+        $eventJson = Get-Content -Raw $eventPath -Encoding utf8
+        $payldHashtables = $payldJson | ConvertFrom-Json -AsHashtable
+        $eventHashtables = $eventJson | ConvertFrom-Json -AsHashtable
+        recursiveEquality $payldHashtables $eventHashtables | Should -Be $true
+    }
+
+    It 'Should only construct a context once' {
+        $context1 = Get-ActionContext
+        $context2 = Get-ActionContext
+
+        $context2 | Should -Be $context1
+        $context2._contextResolveDate | Should -Be $context1._contextResolveDate
+    }
+}
