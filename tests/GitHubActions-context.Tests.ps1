6de41bf607ce5d4068ce0fbb9e18399857e8d05f
Nate Scherer <376408+natescherer@users.noreply.github.com>
2022-11-08 12:41:46 -0500

    Replace AsHashtable on ConvertFrom-Json for Windows PowerShell compatability

--- tests/GitHubActions-context.Tests.ps1
+++ tests/GitHubActions-context.Tests.ps1
@@ -11,6 +11,49 @@ InModuleScope GitHubActions {
 
     Describe 'Get-ActionContext' {
         It 'Should match Real or Faked environment values' {
+            # From https://4sysops.com/archives/convert-json-to-a-powershell-hash-table/, added for Windows PowerShell compatability
+            function ConvertTo-Hashtable {
+                [CmdletBinding()]
+                [OutputType('hashtable')]
+                param (
+                    [Parameter(ValueFromPipeline)]
+                    $InputObject
+                )
+                process {
+                    ## Return null if the input is null. This can happen when calling the function
+                    ## recursively and a property is null
+                    if ($null -eq $InputObject) {
+                        return $null
+                    }
+                    ## Check if the input is an array or collection. If so, we also need to convert
+                    ## those types into hash tables as well. This function will convert all child
+                    ## objects into hash tables (if applicable)
+                    if ($InputObject -is [System.Collections.IEnumerable] -and $InputObject -isnot [string]) {
+                        $collection = @(
+                            foreach ($object in $InputObject) {
+                                ConvertTo-Hashtable -InputObject $object
+                            }
+                        )
+                        ## Return the array but don't enumerate it because the object may be pretty complex
+                        Write-Output -NoEnumerate $collection
+                    }
+                    elseif ($InputObject -is [psobject]) {
+                        ## If the object has properties that need enumeration
+                        ## Convert it to its own hash table and return it
+                        $hash = @{}
+                        foreach ($property in $InputObject.PSObject.Properties) {
+                            $hash[$property.Name] = ConvertTo-Hashtable -InputObject $property.Value
+                        }
+                        $hash
+                    }
+                    else {
+                        ## If the object isn't an array, collection, or other object, it's already a hash table
+                        ## So just return it.
+                        $InputObject
+                    }
+                }
+            }
+
             ## When run within an actual GH Workflow, these values
             ## should already exist, otherwise we fake them out
             if (-not $env:GITHUB_EVENT_NAME) { $env:GITHUB_EVENT_NAME = 'GITHUB_EVENT_NAME' }
@@ -46,7 +89,7 @@ InModuleScope GitHubActions {
             $payload = $context.Payload
 
             $eventJson = Get-Content -Raw $env:GITHUB_EVENT_PATH -Encoding utf8
-            $eventProps = $eventJson | ConvertFrom-Json -AsHashtable
+            $eventProps = $eventJson | ConvertFrom-Json | ConvertTo-Hashtable
             $eventDetail = [pscustomobject]::new()
             AddReadOnlyProps $eventDetail $eventProps
 
