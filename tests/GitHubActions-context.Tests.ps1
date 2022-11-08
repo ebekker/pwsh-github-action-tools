a68a0bd3e0dd61645d235157d2698bfeba3c7cc0
Nate Scherer <376408+natescherer@users.noreply.github.com>
2022-11-08 11:56:55 -0500

    Switch from int parsing to long parsing

--- tests/GitHubActions-context.Tests.ps1
+++ tests/GitHubActions-context.Tests.ps1
@@ -30,9 +30,6 @@ InModuleScope GitHubActions {
 
             $context = Get-ActionContext
 
-            Write-Host "`$context.RunId is '$($context.RunId)'"
-            Write-Host "`$env:GITHUB_RUN_ID is '$($env:GITHUB_RUN_ID)'"
-
             $context.EventName | Should -Be $env:GITHUB_EVENT_NAME
             $context.Sha | Should -Be $env:GITHUB_SHA
             $context.Ref | Should -Be $env:GITHUB_REF
@@ -40,8 +37,8 @@ InModuleScope GitHubActions {
             $context.Action | Should -Be $env:GITHUB_ACTION
             $context.Actor | Should -Be $env:GITHUB_ACTOR
             $context.Job | Should -Be $env:GITHUB_JOB
-            $context.RunNumber | Should -Be ([int]::Parse($env:GITHUB_RUN_NUMBER))
-            $context.RunId | Should -Be ([int]::Parse($env:GITHUB_RUN_ID))
+            $context.RunNumber | Should -Be ([long]::Parse($env:GITHUB_RUN_NUMBER))
+            $context.RunId | Should -Be ([long]::Parse($env:GITHUB_RUN_ID))
 
             ## We want to canonicalize the output formats in way that we can do a deep compare
             ## that only cares about the 'essence' of property names and values, but ignores
